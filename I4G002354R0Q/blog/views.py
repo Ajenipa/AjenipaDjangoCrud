bfa5b5658e46ad8cdb0b01af17e7ef02dbfcc5df
Ajenipa <jamiuajenipa@gmail.com>
2022-06-22 07:04:37 +0100

    second commit

--- I4G002354R0Q/blog/views.py
+++ I4G002354R0Q/blog/views.py
@@ -1,3 +1,28 @@
 from django.shortcuts import render
+from django.views.generic import ListView,CreateView,UpdateView,DeleteView,DetailView
+from .models import Post
 
 # Create your views here.
+class PostListView(ListView):
+    queryset = Post.objects.all()
+class PostCreateView(CreateView):
+    queryset = Post.objects.all()
+    model = Post
+
+    fields = “__all__”
+
+    success_url  = reverse_lazy(“blog:all”)
+class PostDetailView(DetailView):
+    queryset = Post.objects.all()
+    model = Post
+
+    fields = “__all__”
+
+    success_url  = reverse_lazy(“blog:all”)
+class PostDeleteView(DeleteView):
+    queryset = Post.objects.all()
+    model = Post
+
+    fields = “__all__”
+
+    success_url  = reverse_lazy(“blog:all”)

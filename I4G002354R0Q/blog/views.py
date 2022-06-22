2290e39574a88408e675674b43e7f7b323b48913
Ajenipa <jamiuajenipa@gmail.com>
2022-06-22 07:22:53 +0100

    third commit

--- I4G002354R0Q/blog/views.py
+++ I4G002354R0Q/blog/views.py
@@ -4,25 +4,33 @@ from .models import Post
 
 # Create your views here.
 class PostListView(ListView):
+    template_name = "blog/post_list.html"
     queryset = Post.objects.all()
 class PostCreateView(CreateView):
     queryset = Post.objects.all()
-    model = Post
+    #model = Post
 
-    fields = “__all__”
+   # fields = “__all__”
 
-    success_url  = reverse_lazy(“blog:all”)
+    #success_url  = reverse_lazy(“blog:all”)
 class PostDetailView(DetailView):
     queryset = Post.objects.all()
-    model = Post
+    #model = Post
 
-    fields = “__all__”
+    #fields = “__all__”
 
-    success_url  = reverse_lazy(“blog:all”)
+    #success_url  = reverse_lazy(“blog:all”)
+class PostUpdateView(UpdateView):
+    queryset = Post.objects.all()
+    #model = Post
+
+    #fields = “__all__”
+
+    #success_url  = reverse_lazy(“blog:all”)
 class PostDeleteView(DeleteView):
     queryset = Post.objects.all()
-    model = Post
+    #model = Post
 
-    fields = “__all__”
+    #fields = “__all__”
 
-    success_url  = reverse_lazy(“blog:all”)
+    #success_url  = reverse_lazy(“blog:all”)
